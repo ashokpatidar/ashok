df8455e05413f6f36f8a206b6f7b646ac0d03d49
Ashok Patidar <flash.ria.developer@gmail.com>
2022-12-24 18:15:22 -0500

    first for version

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+function say() {
+  return `
+  Hey Ashok Patidar 
+  good to be here for 
+  node cli
+  
+    Twitter: Link
+    Github: https://github.com/ashokpatidar/ashok
+  `;
+}
+say();
+
+console.log(say());
